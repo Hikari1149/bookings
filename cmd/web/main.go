0b6e7820ebe262c410e2bb89002f23e78d7dab6a
Hikari1149 <122094854@qq.com>
2021-10-03 23:53:09 +0800

    [+]: tmpl parseGlob to common layout

---
+++ cmd/web/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Hikari1149/go-web-example/pkg/config"
+	"github.com/Hikari1149/go-web-example/pkg/handlers"
+	"github.com/Hikari1149/go-web-example/pkg/render"
+)
+
+const portNumber = ":8080"
+
+// main in the main application function
+func main() {
+
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+	fmt.Println(fmt.Sprintf("starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Hikari1149/go-web-example
+
+go 1.17
