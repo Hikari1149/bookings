e071ec0b89bdcb6b1bb8f70519ec36d213a319ea
Hikar1149 <122094854@qq.com>
2021-10-12 23:01:26 +0800

    [+]: mock req with session context

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -68,6 +69,62 @@ var theTests = []struct {
 		[]postData{},
 		http.StatusOK,
 	},
+	{
+		"post-search-avail",
+		"/search-availability",
+		"POST",
+		[]postData{
+			{
+				key:   "start",
+				value: "2020-01-01",
+			},
+			{
+				key:   "end",
+				value: "2020-01-02",
+			},
+		},
+		http.StatusOK,
+	},
+	{
+		"post-search-avail-json",
+		"/search-availability-json",
+		"POST",
+		[]postData{
+			{
+				key:   "start",
+				value: "2020-01-01",
+			},
+			{
+				key:   "end",
+				value: "2020-01-02",
+			},
+		},
+		http.StatusOK,
+	},
+	{
+		"make reservation",
+		"/make-reservation",
+		"POST",
+		[]postData{
+			{
+				key:   "first_name",
+				value: "2020-01-01",
+			},
+			{
+				key:   "last_name",
+				value: "2020-01-02",
+			},
+			{
+				key:   "email",
+				value: "414124@qq.com",
+			},
+			{
+				key:   "phone",
+				value: "23123123",
+			},
+		},
+		http.StatusOK,
+	},
 }
 
 func TestHandlers(t *testing.T) {
@@ -89,6 +146,20 @@ func TestHandlers(t *testing.T) {
 
 		} else {
 
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
