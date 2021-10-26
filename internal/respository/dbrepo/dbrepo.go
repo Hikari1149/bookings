343153a11366f08e1437d62038b915ca8df5120b
Hikar1149 <122094854@qq.com>
2021-10-26 23:26:22 +0800

    [+]: db driver to connect

---
+++ internal/respository/dbrepo/dbrepo.go
@@ -0,0 +1,20 @@
+package dbrepo
+
+import (
+	"database/sql"
+
+	"github.com/Hikari1149/bookings/internal/config"
+	repository "github.com/Hikari1149/bookings/internal/respository"
+)
+
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+func NewPostGresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
