d9e1e34a6f65ce2b694b509d505cec0fa36e7ca5
Hikar1149 <122094854@qq.com>
2021-10-31 00:18:48 +0800

    [+]: seeach availiy for all rooms

--- internal/respository/repository.go
+++ internal/respository/repository.go
@@ -1,6 +1,10 @@
 package repository
 
-import "github.com/Hikari1149/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/Hikari1149/bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
@@ -8,4 +12,8 @@ type DatabaseRepo interface {
 	InsertReservation(res models.Reservation) (int, error)
 
 	InsertRoomRestriction(r models.RommRestriction) error
+
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, rooID int) (bool, error)
+
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
