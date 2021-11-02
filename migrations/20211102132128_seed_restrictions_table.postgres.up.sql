58e14259e4190f5d7b1fac6908339f4034b1b30f
Hikar1149 <122094854@qq.com>
2021-11-02 21:22:39 +0800

    [+]: migrate for seeding room & restrictions

---
+++ migrations/20211102132128_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('aaa','2021-03-01 00:00:00.000','2021-03-01 00:00:00.000'),
+	 ('Onwer Block','2022-11-19 00:00:00.000','2022-11-19 00:00:00.000');
